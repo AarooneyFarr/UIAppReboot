e52e297046fe02a245609b2c5e4b1e4524461647
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-28 08:54:45 -0600

    added ignore files

--- UIAppReboot/ViewController.swift
+++ UIAppReboot/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var DynamicView = UIView(frame: CGRectMake(100,100,100,100))
+    DynamicView.backgroundColor = UIColor.greenColor()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
